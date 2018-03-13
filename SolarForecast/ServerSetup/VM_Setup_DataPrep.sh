624de7a633d9fee110bbb633dd1df1a9894c4fcd
bheff88 <braden.heffernan@yahoo.ca>
2018-03-13 12:09:44 -0600

    a

--- SolarForecast/ServerSetup/VM_Setup_DataPrep.sh
+++ SolarForecast/ServerSetup/VM_Setup_DataPrep.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-wget https://repo.continuum.io/archive/Anaconda3-4.2.0-Linux-x86_64.sh
+wget -nv https://repo.continuum.io/archive/Anaconda3-4.2.0-Linux-x86_64.sh
 bash Anaconda3-4.2.0-Linux-x86_64.sh -b -p ~/anaconda3
 rm Anaconda3-4.2.0-Linux-x86_64.sh
 echo 'export PATH="~/anaconda3/bin:$PATH"' >> ~/.bashrc
 source ~/.bashrc
 ./anaconda3/bin/conda update -y conda
-./anaconda3/bin/conda install -y scipy h5py networkx
+./anaconda3/bin/conda create -y -n CPU python=3.5 pickleshare pandas scipy h5py networkx
 sudo apt install make gcc gfortran libncurses-dev
 wget https://cdaweb.gsfc.nasa.gov/pub/software/cdf/dist/latest-release/linux/cdf36_4-dist-all.tar.gz
 tar xf cdf36_4-dist-all.tar.gz
@@ -17,4 +17,6 @@ echo 'export CDF_INC=$CDF_BASE/include' >> ~/.bashrc
 echo 'export CDF_LIB=$CDF_BASE/lib' >> ~/.bashrc
 echo 'export LD_LIBRARY_PATH=$HOME/cdf-dist/lib:$LD_LIBRARY_PATH' >> ~/.bashrc
 source ~/.bashrc
+source ./anaconda3/bin/activate CPU
+rm -rf *.gz
 pip install spacepy
